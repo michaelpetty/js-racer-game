63f82cd2bec3018ea609659139cee9502098a58c
mpetty <michael.petty@gmail.com>
2019-03-28 00:58:00 -0700

    version 1

---
+++ starter-code/app.js
@@ -0,0 +1,40 @@
+const player1 = document.getElementById('1');
+let positionPlayer1 = 1;
+const player2 = document.getElementById('2');
+let positionPlayer2 = 1;
+const totalSpaces = document.getElementsByClassName('space').length/2;
+
+document.addEventListener('keydown', whichKey);
+
+const startModal = document.getElementById('start');
+
+startModal.addEventListener('click', function() {this.style.display = 'none';});
+
+
+function whichKey(e) {
+  if (e.keyCode === 190) {
+    movePlayerOne();
+  } else if (e.keyCode === 90) {
+    movePlayerTwo();
+  }
+}
+
+function movePlayerOne() {
+  positionPlayer1++;
+  moveOneSpace(1, positionPlayer1, player1);
+}
+function movePlayerTwo() {
+  positionPlayer2++;
+  moveOneSpace(2, positionPlayer2, player2);
+}
+function moveOneSpace(playerNum, position, player ) {
+  document.getElementById(playerNum+'-space'+position).append(player);
+  if (position === totalSpaces) {
+    document.removeEventListener('keydown', whichKey);
+    winner(playerNum);
+  }
+}
+
+function winner(num) {
+  console.log(`And the winner is Player ${num}`);
+}
