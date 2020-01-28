17d11519abba506c33e72c375bd20417ec503360
mpetty <michael.petty@gmail.com>
2020-01-28 11:31:25 -0800

    move game logic to Game class

--- js/Game.js
+++ js/Game.js
@@ -1,8 +1,40 @@
 class Game {
-  constructor (player1, player2) {
+  constructor (player1, player2, spaces) {
     this.player1 = player1;
     this.player2 = player2;
+    this.spaces = spaces;
   }
+
+  winner(num) {
+    console.log(`And the winner is Player ${num}`);
+  }
+
+  moveOneSpace(playerNum, position, player ) {
+    document.getElementById(playerNum+'-space'+position).append(player);
+    if (position === this.spaces) {
+      document.removeEventListener('keydown', this.whichKeyL);
+      this.winner(playerNum);
+    }
+  }
+
+  movePlayerOne() {
+    this.player1.pos++;
+    this.moveOneSpace(1, this.player1.pos, this.player1.ele);
+  }
+
+  movePlayerTwo() {
+    this.player2.pos++;
+    this.moveOneSpace(2, this.player2.pos, this.player2.ele);
+  }
+
+  whichKey(e) {
+    if (e.keyCode === 190) {
+      this.movePlayerOne();
+    } else if (e.keyCode === 90) {
+      this.movePlayerTwo();
+    }
+  }
+
 }
 
 export default Game;
--- js/app.js
+++ js/app.js
@@ -5,44 +5,15 @@ let testPl = new Player('rand', 'm5.png', 1);
 let testPl2 = new Player('comp', 'default', 2);
 console.log(testPl.name, testPl.avi);
 
-let curGame = new Game(testPl, testPl2);
-console.log(curGame);
-
-let positionPlayer1 = 1;
-let positionPlayer2 = 1;
 const totalSpaces = document.getElementsByClassName('space').length/2;
 
-document.addEventListener('keydown', whichKey);
+let curGame = new Game(testPl, testPl2, totalSpaces);
+console.log(curGame);
 
 const startModal = document.getElementById('start');
 
-startModal.addEventListener('click', function() {this.style.display = 'none';});
-
-
-function whichKey(e) {
-  if (e.keyCode === 190) {
-    movePlayerOne();
-  } else if (e.keyCode === 90) {
-    movePlayerTwo();
-  }
-}
-
-function movePlayerOne() {
-  positionPlayer1++;
-  moveOneSpace(1, positionPlayer1, testPl.ele);
-}
-function movePlayerTwo() {
-  positionPlayer2++;
-  moveOneSpace(2, positionPlayer2, testPl2.ele);
-}
-function moveOneSpace(playerNum, position, player ) {
-  document.getElementById(playerNum+'-space'+position).append(player);
-  if (position === totalSpaces) {
-    document.removeEventListener('keydown', whichKey);
-    winner(playerNum);
-  }
-}
-
-function winner(num) {
-  console.log(`And the winner is Player ${num}`);
-}
+startModal.addEventListener('click', function() {
+  this.style.display = 'none';
+  curGame.whichKeyL = curGame.whichKey.bind(curGame);
+  document.addEventListener('keydown', curGame.whichKeyL);
+});
